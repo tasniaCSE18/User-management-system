7558490ab026a1967bbd7086356e9be56d257870
tasniaCSE18 <78928304+tasniaCSE18@users.noreply.github.com>
2024-03-08 00:25:35 +0600

    Create index.php

---
+++ Bou Code/index.php
@@ -0,0 +1,30 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+</head>
+
+ 
+<body >
+
+    <form action="insert.php" method="POST">
+
+        <input type="text"  name="n1" > 
+        <br>
+        <input type="int"  name="n2" >
+         <br>
+        <input type="submit" value="Submit" name="submit" >
+        <br>
+        <h1><a href="display.php">Display button</a></h1> 
+
+        
+    </form>
+
+</body>
+
+
+</html>
